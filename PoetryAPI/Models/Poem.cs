fef23dd2e29120ac035f0799aa4537be98cfe952
Aaron Hooper <aaronhoopermail@gmail.com>
2022-08-07 18:00:56 +1200

    Add data service PoemService to provide in-memory data caching service

---
+++ PoetryAPI/Models/Poem.cs
@@ -0,0 +1,10 @@
+﻿namespace PoetryAPI.Models
+{
+    public class Poem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string[] Lines { get; set; }
+    }
+}
---
+++ PoetryAPI/Services/PoemService.cs
@@ -0,0 +1,55 @@
+﻿using PoetryAPI.Models;
+
+namespace PoetryAPI.Services
+{
+    public static class PoemService
+    {
+        static List<Poem> Poems { get; }
+        static int nextId = 0;
+        static PoemService()
+        {
+            Poems = new List<Poem>
+            {
+            };
+        }
+
+        public static List<Poem> GetAll() => Poems;
+
+        public static Poem? Get(int id) => Poems.FirstOrDefault(p => p.Id == id);
+
+        public static Poem? Get(string title) => Poems.FirstOrDefault(p => p.Title == title);
+
+        public static void Add(Poem poem)
+        { 
+            poem.Id = nextId++;
+            Poems.Add(poem);
+        }
+
+        public static void Delete(int id)
+        {
+            var poem = Get(id);
+            if (poem is null)
+                return;
+
+            Poems.Remove(poem);
+        }
+
+        public static void Delete(string title)
+        {
+            var poem = Get(title);
+            if (poem is null)
+                return;
+
+            Poems.Remove(poem);
+        }
+
+        public static void Update(Poem poem)
+        {
+            var index = Poems.FindIndex(p => p.Id == poem.Id);
+            if (index == -1)
+                return;
+
+            Poems[index] = poem;
+        }
+    }
+}
